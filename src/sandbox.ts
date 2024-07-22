f25667d5351c8849ac59150f885c558105bf2f1b
oshanDMOA <capitalassignments67@gmail.com>
2024-07-22 14:00:14 +0530

    Feat: AddStringVariable

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -8,8 +8,17 @@ greet = () =>{
     console.log("Hello, World");
 }
 
-const add = (a:number,b:number,c?:number | string) => {
+const add = (a:number,b:number,c:number | string = 10) => {
     console.log(a+b);
+    console.log(c);
 }
 
-add(2,3);
+add(5,10,'20');
+
+
+
+const minus = (a:number,b:number) : number=>{
+    return a+b;
+}
+
+let results = minus(3,5);
